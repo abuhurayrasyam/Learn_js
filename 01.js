655381c44174bf1716f6de2d0c92d16b141a0b9c
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-02-04 22:10:28 +0600

    basic js

---
+++ 01.js
@@ -0,0 +1,30 @@
+//Task-01
+var deposit = 1000;
+var applePrice = 220;
+var orangePrice = 300;
+var oneKgAppleAndOrangePrice = applePrice + orangePrice;
+
+console.log(oneKgAppleAndOrangePrice)
+console.log(deposit - oneKgAppleAndOrangePrice)
+
+//Task-02
+var mathematics = 90.5052;
+var biology = 81.1011;
+var chemistry = 83.506070;
+var physics = 85.113355;
+var bangla = 95;
+
+console.log(mathematics.toFixed(2), biology.toFixed(2), chemistry.toFixed(2), physics.toFixed(2), bangla.toFixed(2))
+
+//Task-03
+var number = 112;
+number %= 5;
+console.log(number)
+
+//Task-04
+var a = isNaN('11');
+var a = isNaN('2-10');
+
+
+
+
