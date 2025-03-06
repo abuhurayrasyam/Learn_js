54dadf2b22b061cb8a2fb7c3d3c683170bad543d
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-03-07 01:57:33 +0600

    basic JS

---
+++ 01.Basic JS/01.variable.js
@@ -0,0 +1,32 @@
+//***//variable: ভ্যারিয়েবল হল একটি স্থান যেখানে আপনি ডেটা সংরক্ষণ করতে পারেন। জাভাস্ক্রিপ্টে, ভ্যারিয়েবল ডিক্লার করার জন্য var, let, বা const কীওয়ার্ড ব্যবহার করা হয়। এবং এটি ক্যামেল কেসে ডিক্লার করা হয়।
+
+//Task-01
+var deposit = 1000;
+var applePrice = 220;
+var orangePrice = 300;
+var oneKgAppleAndOrangePrice = applePrice + orangePrice;
+
+console.log(oneKgAppleAndOrangePrice)
+console.log(deposit - oneKgAppleAndOrangePrice)
+
+//Task-02
+var mathematics = 90.5052;
+var biology = 81.1011;
+var chemistry = 83.506070;
+var physics = 85.113355;
+var bangla = 95;
+
+console.log(mathematics.toFixed(2), biology.toFixed(2), chemistry.toFixed(2), physics.toFixed(2), bangla.toFixed(2))
+
+//Task-03
+var number = 112;
+number %= 5;
+console.log(number)
+
+//Task-04
+var a = isNaN('11');
+var a = isNaN('2-10');
+
+
+
+
---
+++ 01.Basic JS/07.function.js
@@ -0,0 +1,106 @@
+//***//ফাংশন (Function) :  ফাংশন হল একটি পুনঃব্যবহারযোগ্য কোড ব্লক যা নির্দিষ্ট কাজ সম্পাদন করে। ফাংশন ডিফাইন করার জন্য function কীওয়ার্ড বা অ্যারো ফাংশন ব্যবহার করা হয়।এবং এটি ক্যামেল কেসে ডিক্লার করা হয়।
+
+function cashOut( money ) {
+    if (typeof money !== "number" || money <= 0){
+        return "Invalid";
+    }
+    else{
+        const charge = money * 1.75 / 100;
+        return charge;
+    }
+}
+
+
+
+function  validEmail( email ) {
+    if (typeof email !== "string"){
+        return "Invalid"
+    }
+    else{
+        let spacialCharacters = [".", "-", "_", "+", "@"];
+        if (!spacialCharacters.includes(email.charAt(0)) && email.includes("@") && !email.includes(" ") && email.endsWith(".com")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+
+
+function  electionResult( votes ) {
+    if (!Array.isArray(votes)){
+        return "Invalid";
+    }
+    else{
+        let mangoParty = 0;
+        let bananaParty = 0;
+
+        for (let vote of votes){
+            if (vote === "mango"){
+                mangoParty++;
+            }
+            if (vote === "banana"){
+                bananaParty++;
+            }
+        }
+
+        if (mangoParty > bananaParty){
+            return "Mango";
+        }
+        else if (bananaParty > mangoParty){
+            return "Banana";
+        }
+        else{
+            return "Draw";
+        }
+    }
+}
+
+
+
+function  isBestFriend( f1 , f2 ) {
+    if(typeof f1 !== "object" || typeof f2 !== "object"){
+        return "Invalid";
+    }
+    else{
+        if (!f1.hasOwnProperty("name") || !f1.hasOwnProperty("roll") || !f1.hasOwnProperty("bestFriend") || !f2.hasOwnProperty("name")|| !f2.hasOwnProperty("roll") || !f2.hasOwnProperty("bestFriend")){
+            return "Invalid";
+        }
+        else{
+            if(f1.roll === f2.bestFriend && f2.roll === f1.bestFriend){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+}
+
+
+
+function  calculateWatchTime( times ) {
+    let fullTimesSum = 0;
+    for (let time of times){
+        if (typeof time !== "number"){
+            return "Invalid";
+        }
+        else{
+            fullTimesSum += time;
+        }
+    }
+    
+    let hours = Math.floor(fullTimesSum / 3600);
+    let remindingSeconds = fullTimesSum % 3600;
+    let minutes = Math.floor(remindingSeconds / 60);
+    let seconds = remindingSeconds % 60;
+
+    let totalWatchTimes = {
+        hour: hours,
+        minute: minutes,
+        second: seconds
+    };
+    return totalWatchTimes;
+}
