8d24e931a6c38fe52d38b8794fa2fb283dc445ee
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-03-06 21:24:51 +0600

    basic ES6

--- DOM and Intermediate JS and API/01.Basic-ECMA Script 6/04.spread-operator.js
+++ DOM and Intermediate JS and API/01.Basic-ECMA Script 6/04.spread-operator.js
@@ -10,3 +10,11 @@ console.log(Math.max(numbers)); //output: NaN
 //উপরোক্ত আউটপুটে যখন Math.max(numbers) ব্যবহার করা হয়েছে, তখন এটি অ্যারেকে একটি সংখ্যা হিসাবে গণ্য করার চেষ্টা করেছে, যা সঠিক নয় এবং তাই এটি NaN (Not-a-Number) রিটার্ন করেছে। কারণ numbers একটি অ্যারে, এবং Math.max() ফাংশনটি ব্যবহার করে একটি একক সংখ্যা খোঁজা হচ্ছে।
 console.log(Math.max(...numbers)); //output: 40
 //উপরোক্ত আউটপুটে spread operator ব্যবহার করার কারণে প্রথমে অপারেটরটি array থেকে সমস্ত উপাদান বের করে আনছে, অতঃপর Math.max() এর মাধ্যমে প্রত্যেকটি উপাদানের মধ্যে তুলনা করে সর্বোচ্চ উপাদানটি আউটপুট হিসাবে প্রদান করা হচ্ছে।
+
+//example 03: একটি array এর এলিমেন্ট গুলোকে কপি করে অন্য আরেকটি array তে রাখা হচ্ছে।
+const copyCountries = [...countries]; //example ০১ এর আরে কে কপি করা হয়েছে।
+console.log(copyCountries); //output: [ 'BD', 'PAK' ]
+
+//example 04: দুটি array কে যোগ করা হচ্ছে।
+const sumOfTwoArrays = [...numbers, ...copyCountries]; //example ০২ ও ০৩ এর array কে যোগ করা হয়েছে।
+console.log(sumOfTwoArrays); //output: [ 10, 20, 5, 40, 'BD', 'PAK' ]
